b833e684409a3663077bb0a9191baa1c0dd0d0e9
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-07-11 01:00:08 +0700

    11.07.20
    Навигационна панель расположена при помощи flex, помечена активная страница

--- lectures/lectio 1/js/script.js
+++ lectures/lectio 1/js/script.js
@@ -1,3 +1,16 @@
+$(function () {
+    var location = window.location.href; //url текущей страницы
+    var cur_url =  location.split('/').pop();
+ 
+    $('.menu li').each(function () {
+        var link = $(this).find('a').attr('href');
+ 
+        if (cur_url == link) {
+            $(this).addClass('current');
+        }
+    });
+});
+
 
  //кнопка наверх
 $(function() {
